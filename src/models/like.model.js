1994fe3998faee84ac0d24077693fc2a33094c2f
TheResilientCoder <syedasim2021@gmail.com>
2025-10-27 08:16:44 +0500

    feat: add models for comments, likes, playlists, tweets, and implement Mongoose schemas

---
+++ src/models/like.model.js
@@ -0,0 +1,27 @@
+import mongoose, {Schema, Types} from "mongoose";
+
+const likeSchema = new Schema (
+    {
+        comments:{
+            type : Schema.Types.ObjectId,
+            ref : "Comment"
+        },
+        video: [
+            {
+                type : Schema.Types.ObjectId,
+                ref : "Video"
+            }
+        ],
+
+        likedby : { 
+            type : Schema.Types.ObjectId,
+            ref: "User"
+        },
+
+        tweet :{
+            type: Schema.Types.ObjectId,
+            ref : "Tweet"
+        }
+
+    },{timestamps : true}
+)
