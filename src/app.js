b6137709b753d5c732414f6fdc3a8fb4bc600064
TheResilientCoder <syedasim2021@gmail.com>
2025-11-07 00:56:24 +0500

    feat: add healthcheck and dashboard routes with corresponding controllers for channel stats and videos

--- src/app.js
+++ src/app.js
@@ -29,6 +29,8 @@ import videoRouter from "./routes/video.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
+import  healthcheckRouter from "./routes/healthcheck.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 // use below route for users related routes regestor login etc 
 app.use("/api/v1/users" , userRouter)
 
@@ -43,6 +45,10 @@ app.use("/api/v1/playlist" ,playlistRouter)
 app.use("/api/v1/likes" ,likeRouter)
 
 app.use("/api/v1/subscriptions" , subscriptionRouter)
+
+app.use("/api/v1/healthcheck" , healthcheckRouter)
+
+app.use("/api/v1/dashboard" , dashboardRouter)
 
 
 
