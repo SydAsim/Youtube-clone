ae0b6731d26576625eecbb2f8ff01a25742d5220
TheResilientCoder <syedasim2021@gmail.com>
2025-11-19 23:39:46 +0500

    chore: Clean up proxy configuration comments in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
+  base: '/',   // <-- REQUIRED FOR DEPLOYMENT
+  server: {
     port: 5173,
-    proxy:{ 
-      // Proxy configuration to avoid CORS issues
-      // Comment out for development with Fly.io- backend
-      // Uncomment if running local backend on port 8000
-      
+    proxy: {
       // '/api': {
-      //   target: 'http://localhost:8000',  // Backend server URL
-      //   changeOrigin: true,  // Changes the origin of the host header to the target URL
-      //   secure: false,  // If backend uses HTTPS with self-signed certificate
+      //   target: 'http://localhost:8000',
+      //   changeOrigin: true,
+      //   secure: false,
       // }
     }
   },
